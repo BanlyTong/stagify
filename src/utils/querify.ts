3af133c8dc3c312c47508042c6a3978fd5ab2a6f
BanlyTong <banlytong@gmail.com>
2023-05-22 22:05:39 +0700

    feat: allow pass object param

--- src/utils/querify.ts
+++ src/utils/querify.ts
@@ -3,7 +3,7 @@ import { ICondition, IFilter, mapFilters } from './map_filter';
 import { mapOptions } from './map_options';
 
 export interface IProject {
-  [key: string]: number
+  [key: string]: number | boolean;
 }
 
 interface IFilters {
@@ -11,30 +11,51 @@ interface IFilters {
 }
 
 interface ISort {
-  [key: string]: number
+  [key: string]: number;
 }
 interface IOption {
-  sort?: ISort
-  limit?: number | string
-  skip?: number | string
+  sort?: ISort;
+  limit?: number | string;
+  skip?: number | string;
 }
 
 export interface IQuerify {
-  filters: IFilters
-  select: IProject
-  options: IOption
+  filters: IFilters;
+  select: IProject;
+  options: IOption;
 }
 
-export default (query: string): IQuerify => {
-  const { select, page, limit, sort, ...params }:any = qs.parse(query);
-  
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+export interface IQuery {
+  select: IProject;
+  page: number;
+  sort: ISort;
+  limit: number;
+  [key: string]: IProject | number | ISort | string | string[];
+}
+
+export function querify(q: string | IQuery): IQuerify {
+  let query;
+  if (typeof q === 'string') {
+    query = qs.parse(q);
+  } else {
+    query = q;
+  }
+
+  const { select, page, limit, sort, ...params } = query;
+
   const filters: ICondition = mapFilters(params as IFilter);
   const options = mapOptions(page as string, limit as string, sort as string);
   const project: IProject = {};
-  for (const field in select) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    project[field] = parseInt(select[field] as string, 10);
+
+  if (typeof select === 'object' && !Array.isArray(select)) {
+    for (const field in select) {
+      if (typeof select[field] === 'string') {
+        const str = select[field];
+        if (typeof str === 'string') {
+          project[field] = parseInt(str, 10);
+        }
+      }
+    }
   }
 
   return {
@@ -42,4 +63,6 @@ export default (query: string): IQuerify => {
     select: project,
     options,
   };
-};
+}
+
+export default querify;
